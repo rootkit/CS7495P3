d40e67e088c4d3c78a60e37ed30a398fc59408ca
Andrew Price <arprice@gatech.edu>
2013-12-02 17:31:14 -0500

    Full BoW example finished. It thinks everything's an airplane...

--- test/unit/TestBOW.cpp
+++ test/unit/TestBOW.cpp
@@ -45,7 +45,17 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/features2d/features2d.hpp>
+#include <opencv2/nonfree/features2d.hpp>
 #include <opencv2/nonfree/nonfree.hpp>
+#include <opencv2/ml/ml.hpp>
+
+#include <boost/filesystem.hpp>
+
+const int BOW_FEATURE_SIZE = 5;
+const std::string BASE_PATH = "/home/arprice/Desktop/bow/";
+const std::string TEMPLATES_PATH = "templates/";
+const std::string TRAINING_PATH = "training/";
+const std::string TESTING_PATH = "testing/";
 
 class TestBoW : public CppUnit::TestFixture
 {
@@ -63,34 +73,218 @@ public:
 
 	virtual void tearDown () {}
 
-	cv::Mat trainVocabulary(const std::vector<cv::Mat>& templates,
+	cv::Mat buildVocabulary(boost::filesystem3::directory_iterator& iter,
 							const cv::Ptr<cv::FeatureDetector> detector,
 							const cv::Ptr<cv::DescriptorExtractor> extractor,
-							cv::Ptr<cv::BOWTrainer> trainer)
+							cv::Ptr<cv::BOWTrainer> trainer,
+							const bool lookForSavedVocab = true,
+							const bool saveTrainedVocab = true)
 	{
+		cv::Mat vocabulary;
+		std::string parentPath = iter->path().parent_path().string();
+		std::string vocabPath = parentPath + "/vocab.xml";
+
+		// Look for pretrained vocabulary
+		if (lookForSavedVocab && boost::filesystem3::exists(vocabPath))
+		{
+			std::cout << "Loading filesystem vocabulary: " << vocabPath << std::endl;
+			cv::FileStorage fs(iter->path().parent_path().string() + "/vocab.xml", cv::FileStorage::READ);
+			fs["vocabulary"] >> vocabulary;
+			fs.release();
+
+			return vocabulary;
+		}
+
 		// Use templates to train a vocabulary
 		cv::Mat desc;
-		for(cv::Mat temp : templates)
+		for (boost::filesystem3::directory_iterator end; iter != end; ++iter)
 		{
+			if (iter->path().filename().string() == "vocab.xml") {continue;}
+			std::string filename = parentPath + "/" + iter->path().filename().string();
+			cv::Mat temp = cv::imread(filename);
+
+			std::cout << "Loaded: " << filename << " (" << temp.rows << "x" << temp.cols << ")" << std::endl;
 			std::vector<cv::KeyPoint> kp;
 			detector->detect(temp, kp);
 			extractor->compute(temp, kp, desc);
 			trainer->add(desc);
 		}
+		std::cout << "Clustering...";
+		vocabulary = trainer->cluster();
+		std::cout << "Done!" << std::endl;
 
-		return trainer->cluster();
+		// Save the vocabulary
+		if (saveTrainedVocab)
+		{
+			cv::FileStorage fs(vocabPath, cv::FileStorage::WRITE);
+			fs << "vocabulary" << vocabulary;
+			fs.release();
+		}
+
+		return vocabulary;
+	}
+
+	std::map<std::string, cv::SVM> trainSVMs(const std::string& trainingPath,
+											 const cv::Ptr<cv::FeatureDetector> detector,
+											 cv::Ptr<cv::BOWImgDescriptorExtractor> imgExtractor,
+											 const bool lookForSavedSVMs = true,
+											 const bool saveTrainedSVMs = true)
+	{
+		std::map<std::string, cv::SVM> svms;
+		std::map<std::string, cv::Mat> posFeatures, negFeatures;
+		std::vector<std::string> categories;
+
+		boost::filesystem3::recursive_directory_iterator trainDir(trainingPath);
+
+		// Load categories and positive samples
+		std::string currentCategory;
+		for(boost::filesystem3::recursive_directory_iterator end_iter; trainDir != end_iter; ++trainDir)
+		{
+			if(trainDir.level() == 0)
+			{
+				// Get category name from name of the folder
+				currentCategory = (trainDir->path()).filename().string();
+				categories.push_back(currentCategory);
+
+				svms.insert(std::pair<std::string, cv::SVM>(currentCategory, cv::SVM()));
+				std::cerr << currentCategory << std::endl;
+
+				if (lookForSavedSVMs && boost::filesystem3::exists(trainingPath + currentCategory + "/svm.xml"))
+				{
+					svms[currentCategory].load((trainingPath + currentCategory + "/svm.xml").c_str());
+					std::cerr << svms[currentCategory].get_support_vector_count() << std::endl;
+				}
+				else
+				{
+					posFeatures.insert(std::pair<std::string, cv::Mat>(currentCategory, cv::Mat()));
+					negFeatures.insert(std::pair<std::string, cv::Mat>(currentCategory, cv::Mat()));
+				}
+			}
+			else
+			{
+				if ((trainDir->path()).filename().string() == "Thumbs.db") {continue;}
+				if ((trainDir->path()).filename().string() == "svm.xml") {continue;}
+				if (svms[currentCategory].get_support_vector_count() > 0) {continue;} // SVM already trained.
+
+				// File name with path
+				std::string filename = trainDir->path().parent_path().string() + std::string("/")
+						+ (trainDir->path()).filename().string();
+
+				cv::Mat tmp = cv::imread(filename);
+				cv::Mat desc;
+
+				// Compute the actual feature
+				std::vector<cv::KeyPoint> kp;
+				detector->detect(tmp, kp);
+				imgExtractor->compute(tmp, kp, desc);
+
+				std::cout << "Creating feature: " << filename << " (" << desc.rows << "x" << desc.cols << ")" << std::endl;
+
+				posFeatures[currentCategory].push_back(desc);
+			}
+		}
+
+		// Create negative samples from the positive samples of other classes
+		for (std::string category : categories)
+		{
+			if (svms[currentCategory].get_support_vector_count() > 0) {continue;} // SVM already trained.
+
+			for (std::map<std::string, cv::Mat>::iterator iter = posFeatures.begin();
+				 iter != posFeatures.end(); ++iter)
+			{
+				if (iter->first != category)
+				{
+					std::cerr << "Adding " << iter->first << std::endl;
+					negFeatures[category].push_back(iter->second);
+				}
+				else
+				{
+					std::cerr << "Not Adding " << iter->first << std::endl;
+				}
+			}
+		}
+
+		// Train SVMs for each class
+		for (std::string category : categories)
+		{
+			if (svms[currentCategory].get_support_vector_count() > 0) {continue;} // SVM already trained.
+
+			cv::Mat trainData;
+			cv::Mat trainLabels;
+
+			std::cout << "Creating +/- training data for " << category << "...";
+			cv::vconcat(posFeatures[category],
+						negFeatures[category],
+						trainData);
+			cv::vconcat(cv::Mat::ones(posFeatures[category].rows, 1, CV_32S), // Positive training data has label 1
+						cv::Mat::zeros(negFeatures[category].rows, 1, CV_32S), // Negative training data has label 0
+						trainLabels);
+
+			svms[category].train(trainData, trainLabels);
+			std::cout << "Done." << std::endl;
+		}
+
+		// Save svms
+		if (saveTrainedSVMs)
+		{
+			for (std::map<std::string, cv::SVM>::iterator iter = svms.begin();
+				 iter != svms.end(); ++iter)
+			{
+				std::string svmFilename = BASE_PATH + TRAINING_PATH + iter->first + "/svm.xml";
+				iter->second.save(svmFilename.c_str());
+			}
+		}
+
+		return svms;
+
 	}
 
 	void TestCluster()
 	{
-		cv::Ptr<cv::FeatureDetector> detector(new cv::SiftFeatureDetector());
-		cv::Ptr<cv::DescriptorExtractor> extractor(new cv::SiftDescriptorExtractor());
+		cv::Ptr<cv::FeatureDetector> detector(new cv::SurfFeatureDetector());
+		cv::Ptr<cv::DescriptorExtractor> extractor(new cv::SurfDescriptorExtractor());
 		cv::Ptr<cv::DescriptorMatcher> matcher(new cv::FlannBasedMatcher());
 
-		cv::Ptr<cv::BOWTrainer> trainer(new cv::BOWKMeansTrainer(5));
+		cv::Ptr<cv::BOWTrainer> trainer(new cv::BOWKMeansTrainer(BOW_FEATURE_SIZE));
 		cv::Ptr<cv::BOWImgDescriptorExtractor> imgExtractor(new cv::BOWImgDescriptorExtractor(extractor, matcher));
 
 
+		boost::filesystem3::directory_iterator templateDir(BASE_PATH + TEMPLATES_PATH);
+
+		cv::Mat vocabulary = buildVocabulary(templateDir, detector, extractor, trainer, false);
+		std::cout << "Vocab: " << " (" << vocabulary.rows << "x" << vocabulary.cols << ")" << std::endl;
+
+		imgExtractor->setVocabulary(vocabulary);
+
+		std::map<std::string, cv::SVM> svms = trainSVMs(BASE_PATH + TRAINING_PATH, detector, imgExtractor, false);
+
+		// Do the testing set
+		boost::filesystem3::directory_iterator testIter(BASE_PATH + TESTING_PATH);
+		for (boost::filesystem3::directory_iterator end; testIter != end; ++testIter)
+		{
+			// Compute BoW feature for image
+			std::string filename = testIter->path().parent_path().string() + std::string("/")
+					+ (testIter->path()).filename().string();
+
+			cv::Mat tmp = cv::imread(filename);
+			cv::Mat desc;
+
+			// Compute the actual feature
+			std::vector<cv::KeyPoint> kp;
+			detector->detect(tmp, kp);
+			imgExtractor->compute(tmp, kp, desc);
+
+			std::cout << (testIter->path()).filename().string() << ":" << std::endl;
+
+			// Classify it
+			for (std::map<std::string, cv::SVM>::iterator iter = svms.begin();
+				 iter != svms.end(); ++iter)
+			{
+				std::cout << "\t" <<iter->first << ": " << iter->second.predict(desc) << std::endl;
+			}
+
+
+		}
 	}
 };
 
